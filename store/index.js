5bf867cf7e35a064873b2f8365ac5f6354ec7dfa
ColinTravis <colint@cybertravis.us>
2020-06-17 14:45:50 -0500

    Added some new CMS stuff

--- store/index.js
+++ store/index.js
@@ -1,25 +1,40 @@
 export const state = () => ({
-  blogPosts: []
+  blogPosts: [],
+  cities: []
 })
 
 export const mutations = {
   setBlogPosts(state, list) {
     state.blogPosts = list
+  },
+  setCity(state, list) {
+      state.cities = list
   }
 }
 
 export const actions = {
   async nuxtServerInit({ commit }) {
-    let files = await require.context(
+    let blogFiles = await require.context(
       '~/assets/content/blog/',
       false,
       /\.json$/
     )
-    let blogPosts = files.keys().map(key => {
-      let res = files(key)
+    let cityFiles = await require.context(
+      '~/assets/content/cities/',
+      false,
+      /\.json$/
+    )
+    let blogPosts = blogFiles.keys().map(key => {
+      let res = blogFiles(key)
+      res.slug = key.slice(2, -5)
+      return res
+    })
+    let citiesPosts = cityFiles.keys().map(key => {
+      let res = cityFiles(key)
       res.slug = key.slice(2, -5)
       return res
     })
     await commit('setBlogPosts', blogPosts)
+    await commit('setCities', citiesPosts)
   }
 }
