2f573c2f5f3ae7a49160074a19251a9a7c5200ad
ColinTravis <colint@cybertravis.us>
2020-06-17 15:10:22 -0500

    City info

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
-      <h1>Hi these are the blog posts</h1>
-      <!-- <div>{{blogPosts}}</div> -->
-      <div v-for="posts in blogPosts" :key="key">
-          <!-- <p>{{posts}}</p> -->
-          <nuxt-link :to="`${posts.slug}`"><i>{{posts.title}}</i></nuxt-link>
-      </div>
+    <h1>Hi these are the blog posts</h1>
+    <!-- <div>{{blogPosts}}</div> -->
+    <div v-for="posts in blogPosts" :key="posts">
+      <!-- <p>{{posts}}</p> -->
+      <nuxt-link :to="`${posts.slug}`">
+        <i>{{posts.title}}</i>
+      </nuxt-link>
+    </div>
+{{citiesPosts}}
+
+    <div v-for="city in citiesPosts" :key="city">
+        <h1>{{city.cityName}}</h1>
+        <img :src="city.cityIcon" alt="">
+    </div>
   </div>
 </template>
 
@@ -13,12 +21,18 @@
 export default {
   computed: {
     blogPosts() {
-      return this.$store.state.blogPosts;
+      return this.$store.state.blogPosts
     },
-  },
-};
+    citiesPosts() {
+      return this.$store.state.cities
+    }
+  }
+}
 </script>
 
 <style>
-
+body {
+    background-color: darkslategray;
+    color: white;
+}
 </style>
--- store/index.js
+++ store/index.js
@@ -7,8 +7,8 @@ export const mutations = {
   setBlogPosts(state, list) {
     state.blogPosts = list
   },
-  setCity(state, list) {
-      state.cities = list
+  setCities(state, list) {
+    state.cities = list
   }
 }
 
@@ -29,12 +29,12 @@ export const actions = {
       res.slug = key.slice(2, -5)
       return res
     })
-    let citiesPosts = cityFiles.keys().map(key => {
+    let cities = cityFiles.keys().map(key => {
       let res = cityFiles(key)
-      res.slug = key.slice(2, -5)
+      res.slug = res.cityName
       return res
     })
     await commit('setBlogPosts', blogPosts)
-    await commit('setCities', citiesPosts)
+    await commit('setCities', cities)
   }
 }
