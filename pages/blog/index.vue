e958ed2ee331985742f66b1dcc7cc72d7ffd2682
ColinTravis <colint@cybertravis.us>
2020-06-17 14:20:48 -0500

    basic page test stuff

---
+++ pages/blog/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <h1>Hi these are the blog posts</h1>
+      <!-- <div>{{blogPosts}}</div> -->
+      <div v-for="posts in blogPosts" :key="key">
+          <p>{{posts}}</p>
+          <nuxt-link :to="`blog/${posts.slug}`"><i>{{posts.title}}</i></nuxt-link>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    blogPosts() {
+      return this.$store.state.blogPosts;
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
